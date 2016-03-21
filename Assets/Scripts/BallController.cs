a8048594b95c90529965ecd95c38dac0ee58b6e5
Rekzok <cole.stewart_fsj@hotmail.ca>
2016-03-20 19:29:44 -0700

    Version 0.1
    Created initial assets
    Created ball controller

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+    public float moveSpeed = 952f;
+	
+	// Update is called once per frame
+	void Update () {
+        //creates Value for movement to apply force
+        float xMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        //converting movement values into a vector to apply force to object
+        Vector3 force = new Vector3(xMove, 0f, zMove);
+        GetComponent<Rigidbody>().AddForce(force);
+	}
+}
