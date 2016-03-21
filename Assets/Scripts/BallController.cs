23c79677ea7ddbe297aa314b09daafe12f9ade19
PlatypusFries <kevin.martens@live.com>
2016-03-20 19:34:34 -0700

    Version 0.1.1
    - Added comments to BallController.cs

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BallController : MonoBehaviour {
+public class BallController : MonoBehaviour
+{
 
     public float moveSpeed = 952f;
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         //creates Value for movement to apply force
         float xMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
@@ -14,5 +16,7 @@ public class BallController : MonoBehaviour {
         //converting movement values into a vector to apply force to object
         Vector3 force = new Vector3(xMove, 0f, zMove);
         GetComponent<Rigidbody>().AddForce(force);
+
+        //cole loves black cocks
 	}
 }
