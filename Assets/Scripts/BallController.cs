5022f1e84003b3d09e553075c1d8f684283069ee
PlatypusFries <kevin.martens@live.com>
2016-03-26 20:49:38 -0700

    v0.1.2
    - All assets imported from scratch
    - Player can now jump, ball has bounce physics
    - Player's time/score is now tracked and displayed on-screen

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,20 +1,26 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public float moveSpeed = 100f; //how quickly the ball moves laterally
+    public float jumpSpeed = 1000f;
 
-	public float moveSpeed = 952f;
-	
-	// Update is called once per frame
-	void Update ()
-	{
-        //creates Value for movement to apply force
-        float xMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float zMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+    void Update()
+    {
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, GetComponent<Collider>().bounds.size.y / 2);
 
-        //converting movement values into a vector to apply force to object
-        Vector3 force = new Vector3(xMove, 0f, zMove);
+        //check for keypress and calculate movement speed
+        float xMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime * Convert.ToInt32(isGrounded);
+        float zMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime * Convert.ToInt32(isGrounded);
+        
+        Vector3 force = new Vector3(xMove, 0f, zMove); //create force vector and apply horizontal movement
+
+        if (Input.GetButtonDown("Jump") && isGrounded) //Applies jump speed to the force vector if 
+        {
+            force.y = jumpSpeed * Time.deltaTime;
+        }
+
         GetComponent<Rigidbody>().AddForce(force);
-	}
+    }
 }
---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ScoreTracker : MonoBehaviour
+{
+    //initializes required variables
+    private float score;
+    Text scoreText;
+
+    //sets scoreText object to the script's GameObject
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+	void Update ()
+    {
+        //increments score with the time elapsed in seconds
+        score += Time.deltaTime;
+
+        //outputs score rounded to the first decimal place
+        scoreText.text = Math.Round(score, 1).ToString("0.0");
+	}
+}
