9466771a4cdbee966b6ce7960a83219a6ab1f029
PlatypusFries <kevin.martens@live.com>
2016-03-27 16:26:57 -0700

    Added kill plane

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public float moveSpeed = 100f; //how quickly the ball moves laterally
-    public float jumpSpeed = 1000f;
+    public float jumpSpeed = 1000f; //the upward impulse when the player jumps
 
-    void Update()
+    void Update() //movement specific
     {
-        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, GetComponent<Collider>().bounds.size.y / 2);
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, GetComponent<Collider>().bounds.size.y / 2); //checks if ball is touching the ground
 
-        //check for keypress and calculate movement speed
+        //check for keypress and calculate movement speed; ball cannot change direction if in the air.
         float xMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime * Convert.ToInt32(isGrounded);
         float zMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime * Convert.ToInt32(isGrounded);
         
-        Vector3 force = new Vector3(xMove, 0f, zMove); //create force vector and apply horizontal movement
+        Vector3 force = new Vector3(xMove, 0f, zMove); //creates force vector for horizontal movement
 
-        if (Input.GetButtonDown("Jump") && isGrounded) //Applies jump speed to the force vector if 
+        if (Input.GetButtonDown("Jump") && isGrounded) //Applies jump speed to the force vector if jump button pressed and ball is grounded
         {
             force.y = jumpSpeed * Time.deltaTime;
         }
 
-        GetComponent<Rigidbody>().AddForce(force);
+        GetComponent<Rigidbody>().AddForce(force); //adds all of the forces to the physics calculation
     }
 }
---
+++ Assets/Scripts/KillPlane.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class KillPlane : MonoBehaviour {
+
+    // This script sets a position on the y-axis at which the (player) object 
+    // is considered "dead" and the level is restarted
+
+    public float killPlane = -30f;
+
+	void Update () {
+	    if (transform.position.y <= killPlane)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
