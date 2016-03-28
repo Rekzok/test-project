8ec4b76ed9c49734c8ed05824c00b364cb3d9f44
PlatypusFries <kevin.martens@live.com>
2016-03-27 17:45:44 -0700

    Improved platform controller
    Platform now turns back at max angle. Speed of rotation on each axis
    randomized.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,16 +5,25 @@ public class BallController : MonoBehaviour
 {
     public float moveSpeed = 100f; //how quickly the ball moves laterally
     public float jumpSpeed = 1000f; //the upward impulse when the player jumps
+    private bool isGrounded;
+
+    public void OnCollisionEnter()
+    {
+        isGrounded = true;
+    }
+
+    public void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
 
     void Update() //movement specific
     {
-        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, GetComponent<Collider>().bounds.size.y / 2); //checks if ball is touching the ground
-
-        //check for keypress and calculate movement speed; ball cannot change direction if in the air.
+        //check for keypress and calculate movement speed.
         float xMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime * Convert.ToInt32(isGrounded);
         float zMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime * Convert.ToInt32(isGrounded);
         
-        Vector3 force = new Vector3(xMove, 0f, zMove); //creates force vector for horizontal movement
+        Vector3 force = new Vector3(xMove, 0f, zMove); //creates force vector for lateral movement
 
         if (Input.GetButtonDown("Jump") && isGrounded) //Applies jump speed to the force vector if jump button pressed and ball is grounded
         {
--- Assets/Scripts/PlatformTiltController.cs
+++ Assets/Scripts/PlatformTiltController.cs
@@ -3,33 +3,47 @@ using System.Collections;
 
 public class PlatformTiltController : MonoBehaviour {
 
-    float xDirection = 1f; // Determines whether the rotation on the x-axis is positive or negative
-    float zDirection = 1f; // Determines whether the rotation on the z-axis is positive or negative
-    float rotationSpeed = 5; //speed at which the platform tilts
-    float maxAngle = 40f; // The maximum the platform can be angled 
-    float reverseChance = 0.10f; // Precent chance that the direction of rotation will change
+    private float xDirection; // Determines whether the rotation on the x-axis is positive or negative
+    private float zDirection; // Determines whether the rotation on the z-axis is positive or negative
 
-    // Use this for initialization
+    private float xAngle;
+    private float zAngle;
+
+    private float xRand;
+    private float zRand;
+
+    public float rotationSpeed = 5f; //speed at which the platform tilts
+    public float maxAngle = 20f; // The maximum the platform can be angled 
+
+    public float reverseChance = 0.10f; // Precent chance that the direction of rotation will change
+
     void Start ()
     {
+        //Gives the platform a random direction for initial rotation
         xDirection = Random.Range(0, 2) * 2 - 1;
         zDirection = Random.Range(0, 2) * 2 - 1;
+        xRand = Random.value + Random.Range(1, 3);
+        zRand = Random.value + Random.Range(1, 3);
     }
 	
-	// Update is called once per frame
 	void Update ()
     {
-        transform.Rotate(Vector3.forward, xDirection * rotationSpeed * Time.deltaTime);
-        transform.Rotate(Vector3.right, zDirection * rotationSpeed * Time.deltaTime);
+        //rotates the platform a certain amount each frame
+        transform.Rotate(Vector3.right, xDirection * rotationSpeed * xRand * Time.deltaTime);
+        xAngle += xDirection * rotationSpeed * xRand * Time.deltaTime;
+        transform.Rotate(Vector3.forward, zDirection * rotationSpeed * zRand * Time.deltaTime);
+        zAngle += zDirection * rotationSpeed * zRand * Time.deltaTime;
 
-        /*if (transform.rotation.x >= maxAngle || transform.rotation.x <= -maxAngle)
+        //checks if platform has reached its maximum angle and reverses its direction if so
+        if (xAngle >= maxAngle || xAngle <= -maxAngle)
         {
             xDirection = xDirection * -1;
+            xRand = Random.value + Random.Range(1, 3);
         }
-
-        if (transform.rotation.z >= maxAngle || transform.rotation.z <= -maxAngle)
+        if (zAngle >= maxAngle || zAngle <= -maxAngle)
         {
             zDirection = zDirection * -1;
-        }*/
+            zRand = Random.value + Random.Range(1, 3);
+        }
     }
 }
